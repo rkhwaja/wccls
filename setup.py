a152433d2eaef018bf87d64a4cc1c527b226fef1
Rehan Khwaja <rehan@khwaja.name>
2018-07-03 07:36:29 -0700

    Bump version after release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.rst") as f:
 	long_description = f.read()
 
 setup(name="wccls",
-	version="0.8",
+	version="0.9",
 	description="WCCLS scraper",
 	long_description=long_description,
 	author="Rehan Khwaja",
