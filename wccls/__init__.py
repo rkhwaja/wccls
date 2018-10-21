d7a8dc0816dd50ffca9468f600bdb82f17025e4c
Rehan Khwaja <rehan@khwaja.name>
2018-10-20 17:41:11 -0700

    Setup flit

--- wccls/__init__.py
+++ wccls/__init__.py
@@ -1,3 +1,5 @@
+"""Flit requires a module docstring"""
+
 from .wccls import * # pylint: disable=wildcard-import
 from .wccls_desktop import * # pylint: disable=wildcard-import
 from .wccls_mobile import * # pylint: disable=wildcard-import
@@ -6,3 +8,4 @@ from .wccls_mobile import * # pylint: disable=wildcard-import
 Wccls = WcclsMobile
 
 __all__ = wccls.__all__ + wccls_desktop.__all__ + wccls_mobile.__all__ # pylint: disable=undefined-variable
+__version__ = "1.2.0"
