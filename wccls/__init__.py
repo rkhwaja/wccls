3936a22af76725bcee52ef0804c8ea086f28851d
Rehan Khwaja <rehan@khwaja.name>
2018-11-14 19:38:12 -0800

    Switch recommended method to bibliocommons

--- wccls/__init__.py
+++ wccls/__init__.py
@@ -4,6 +4,6 @@ from .wccls_mobile import * # pylint: disable=wildcard-import
 from .bibliocommons import * # pylint: disable=wildcard-import
 
 # mobile works at the moment
-Wccls = WcclsMobile
+Wccls = WcclsBiblioCommons
 
 __all__ = wccls.__all__ + wccls_desktop.__all__ + wccls_mobile.__all__ + bibliocommons.__all__ # pylint: disable=undefined-variable
