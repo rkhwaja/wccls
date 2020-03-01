a7d48ca00f86cb0ec5b2c1f1b4bb4bb3962b3a06
Rehan Khwaja <rehan@khwaja.name>
2020-03-01 00:23:48 -0800

    Test Multnomah County library too

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,7 +6,7 @@ from pprint import pformat
 
 from wccls import MultnomahBiblioCommons, StatusType, Wccls, WcclsBiblioCommons
 
-def CheckOutput(items):
+def CheckOutput(items, prefix):
 	itemsByStatus = {}
 	for status in StatusType:
 		itemsByStatus[status] = 0
@@ -20,20 +20,16 @@ def CheckOutput(items):
 		if item.status == StatusType.Held and item.isOverdrive is True:
 			heldOverdriveCount += 1
 
-	assert checkedOutOverdriveCount == int(environ["WCCLS_COUNT_CHECKEDOUT_OVERDRIVE"]), "Mismatch in CheckedOut Overdrive count"
-	assert heldOverdriveCount == int(environ["WCCLS_COUNT_HELD_OVERDRIVE"]), "Mismatch in Held Overdrive count"
+	assert checkedOutOverdriveCount == int(environ[f"{prefix}_COUNT_CHECKEDOUT_OVERDRIVE"]), "Mismatch in CheckedOut Overdrive count"
+	assert heldOverdriveCount == int(environ[f"{prefix}_COUNT_HELD_OVERDRIVE"]), "Mismatch in Held Overdrive count"
 
 	for status, count in itemsByStatus.items():
-		assert count == int(environ[f"WCCLS_COUNT_{status.name.upper()}"]), f"Mismatch in {status.name} count. {pformat(itemsByStatus)}"
+		assert count == int(environ[f"{prefix}_COUNT_{status.name.upper()}"]), f"Mismatch in {status.name} count. {pformat(itemsByStatus)}"
 
-def test_recommended():
-	wccls = Wccls(login=environ["WCCLS_CARD_NUMBER"], password=environ["WCCLS_PASSWORD"], debug_=False)
-	CheckOutput(wccls.items)
+def test_wccls():
+	library = WcclsBiblioCommons(login=environ["WCCLS_CARD_NUMBER"], password=environ["WCCLS_PASSWORD"], debug_=True)
+	CheckOutput(library.items, "WCCLS")
 
-def test_wccls_bibliocommons():
-	wccls = WcclsBiblioCommons(login=environ["WCCLS_CARD_NUMBER"], password=environ["WCCLS_PASSWORD"], debug_=True)
-	CheckOutput(wccls.items)
-
-def test_multnomah_bibliocommons():
+def test_multnomah():
 	library = MultnomahBiblioCommons(login=environ["MULTNOMAH_CARD_NUMBER"], password=environ["MULTNOMAH_PASSWORD"], debug_=True)
-	CheckOutput(library.items)
+	CheckOutput(library.items, "MULTNOMAH")
